7e96111f32e927db8b8d8e94532aaba3f10c327c
Shivamp <patelshivam677@gmail.com>
2020-04-30 01:11:46 +0530

    Add function added

--- test.py
+++ test.py
@@ -2,4 +2,7 @@ print("Welcome to Python World")
 import keyword
 print("Total Keyword List in Python")
 for i in keyword.kwlist:
-	print(i,end="\t")
+	print(i,end="\t")def add(a,b):
+	print(""[DThe Sum:",a+b)
+
+add(10,20)
